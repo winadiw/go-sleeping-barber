a18dfa1d6f71ff66210ed8bf6b4605d58fa9c142
Winadi Wiratama <winadi.wiratama@dana.id>
2023-06-06 14:43:55 +0700

    Finish project

---
+++ barbershop.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var mx sync.Mutex
+
+type Barbershop struct {
+	ShopCapacity        int
+	HaircutDuration     time.Duration
+	NumberOfBarbers     int
+	BarbersDoneChan     chan bool
+	ClientsChan         chan string
+	Open                bool
+	TotalHappyCustomers int
+}
+
+func (shop *Barbershop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	// each barber has its own routine
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the waiting room to check for clients.", barber)
+
+		for {
+			// if there are no clients, barber sleep
+			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap.", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-shop.ClientsChan
+
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up", client, barber)
+					isSleeping = false
+				}
+				// cut hair
+				shop.cutHair(barber, client)
+			} else {
+				// shop is closed, send the barber home and close goroutine
+				shop.sendBarberHome(barber)
+				return
+			}
+		}
+	}()
+}
+
+func (shop *Barbershop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair!", barber, client)
+	time.Sleep(shop.HaircutDuration)
+	color.Green("%s is finished cutting %s's hair.", barber, client)
+	mx.Lock()
+	shop.TotalHappyCustomers++
+	mx.Unlock()
+}
+
+func (shop *Barbershop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home.", barber)
+	shop.BarbersDoneChan <- true
+}
+
+func (shop *Barbershop) closeShopForDay() {
+	color.Cyan("Closing shop for the day.")
+
+	close(shop.ClientsChan)
+	shop.Open = false
+
+	for a := 1; a < shop.NumberOfBarbers; a++ {
+		<-shop.BarbersDoneChan // block until all barbers are done
+	}
+
+	close(shop.BarbersDoneChan)
+
+	color.Green("---------------------------------------------------------------------")
+	color.Green("The barbershop is now closed for the day, and everyone has gone home.")
+
+}
+
+func (shop *Barbershop) addClient(client string) {
+	// prints out a message
+	color.Green("*** %s arrives!", client)
+
+	if shop.Open {
+		select {
+		case shop.ClientsChan <- client:
+			color.Yellow("%s takes a seat in the waiting room.", client)
+		default: //handles full buffered channels
+			color.Red("The waiting is room is full, so %s leaves.", client)
+		}
+	} else {
+		color.Red("The shop is already closed, so %s leaves", client)
+	}
+}
